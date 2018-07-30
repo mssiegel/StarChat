3da194789f570f8fa85efabf21d062e074850ef7
Moshe Siegel <siegel.moshes@gmail.com>
2018-07-30 18:52:34 -0400

    added randomized pairing

--- index.js
+++ index.js
@@ -14,16 +14,63 @@ app.use(express.static('public'));
 //Socket setup
 const io = socket(server);
 
+const chatQueue = []; // array of sockets waiting to chat
+const rooms = {}; // map socket.id => room
+const names = {}; // map socket.id => name
+const allUsers = {}; // map socket.id => socket
+
+function findPeerForLoneSocket(socket) {
+  if (chatQueue.length) {
+    // somebody is in queue, pair them!
+   const peer = chatQueue.pop();
+   const room = socket.id + '#' + peer.id;
+   // join them both to room
+   peer.join(room);
+   socket.join(room);
+   // register rooms to their names
+   rooms[peer.id] = room;
+   rooms[socket.id] = room;
+   // exchange names between the two of them and start the chat
+   console.log("people have been matched!!")
+   peer.emit('chat start', {'peerName': names[socket.id], 'room':room});
+   socket.emit('chat start', {'peerName': names[peer.id], 'room':room});
+  }
+  else chatQueue.unshift(socket); // queue is empty, add our lone socket
+}
+
 io.on('connection', socket => {
   console.log('made socket connection', socket.id);
 
-  //Handle chat event
-  socket.on('chat', msg => {
-    io.emit('chat', msg)
+  //Handle new person logging in
+  socket.on('new login', userName => {
+    console.log(`${userName} has entered the chatQueue`)
+    names[socket.id] = userName;
+    allUsers[socket.id] = socket;
+    //check if someone is in queue
+    findPeerForLoneSocket(socket, userName);
+  })
+
+  //Handle new chat message
+  socket.on('chat message', msg => {
+    const room = rooms[socket.id];
+    socket.to(room).emit('chat message', msg);
   });
 
-  socket.on('typing', userHandle => {
-    socket.broadcast.emit('typing', userHandle)
+  socket.on('leave room', endChat);
+  socket.on('disconnect', endChat); //copy of 'leave room', may be able to combine functions
+
+  function endChat(){
+    const room = rooms[socket.id];
+    if(room){
+      socket.to(room).emit('chat end');
+      const socketAndPeerIDs = room.split('#');
+      socketAndPeerIDs.forEach(socketID => allUsers[socketID].leave(room));
+    }
+  }
+
+  socket.on('typing', userName => {
+    const room = rooms[socket.id];
+    socket.to(room).emit('typing', userName)
   });
 
 });
--- public/chat.js
+++ public/chat.js
@@ -2,48 +2,89 @@
 const socket = io();
 
 // Query DOM
-const suggestBtn = document.getElementById('suggestBtn');
+const suggestBtn = document.getElementById('suggest-btn');
       message = document.getElementById('message'),
-      handle = document.getElementById('handle'),
-      form = document.getElementById('chatForm'),
+      userName = document.getElementById('user-name'),
+      form = document.getElementById('chat-form'),
+      chatContainer = document.getElementById('chat-container'),
+      chatWindow = document.getElementById('chat-window'),
       output = document.getElementById('output'),
       feedback = document.getElementById('feedback'),
-      chatWindow = document.getElementById('chat-window');
+      chosenChar = document.getElementById('chosen-char'),
+      startChatBtn = document.getElementById('start-chat-btn'),
+      practiceModeBtn = document.getElementById('practice-mode-btn'),
+      endChatBtn = document.getElementById('end-chat-btn'),
+      sendMessageBtn = document.getElementById('send-message-btn'),
+      lookingForSomeone = document.getElementById('looking-for-someone');
 
+
+//suggests a new character - always different than previous one
 suggestBtn.addEventListener('click', suggestNewCharacter);
 
-function suggestNewCharacter(){
+function suggestNewCharacter() {
   let randomChar;
   do {
-    const characterList = ['Dragon slayer', 'Viking Warrior','Leader of a Warrior Army','Laughing Jack', 'Elf with a scowl', 'Wizard with a scarred cheek', 'Beggar with Super Powered Charm', 'Vampire Hunter', 'Alien Warlord', 'Alien with a Green Tail', 'Pirate Captain', 'Sorcerer with a maniacal laugh', 'Maniac Magician', 'Hypnotist Master', 'Defender of the Righteous', 'Job Applicant', 'Warrior Kingdom Rebel'];
+    const characterList = ['Ceo of Whoopee Cushions R Us', 'Elfin Archer', 'Circus Clown with Powers', 'Ballerina Spy', 'Website Builder', 'Mad Prank Artist', 'Mighty Knight', 'Dragon slayer', 'Viking Warrior','Leader of a Warrior Army','Laughing Jack', 'Elf with a scowl', 'Wizard with a scarred cheek', 'Beggar with Super Powered Charm', 'Vampire Hunter', 'Alien Warlord', 'Alien with a Green Tail', 'Pirate Captain', 'Sorcerer with a maniacal laugh', 'Maniac Magician', 'Hypnotist Master', 'Defender of the Righteous', 'Job Applicant', 'Warrior Kingdom Rebel'];
     const randomChoice = Math.floor(Math.random() * characterList.length);
     randomChar = characterList[randomChoice];
-  } while (randomChar === handle.value); // ensures pressing button twice can't give same character as before
-  handle.value = randomChar;
+  } while (randomChar === userName.value);
+  chosenChar.value = randomChar;
+  userName.value = randomChar;
 }
 
-//Emit events
+//EMIT events
+startChatBtn.addEventListener('click', () => {
+  socket.emit('new login', chosenChar.value);
+  startChatBtn.classList.add('hide');
+  lookingForSomeone.innerHTML = "LOOKING FOR SOMEONE TO PAIR YOUR WITH..."
+});
+
 form.addEventListener('submit', (e) => {
-  socket.emit('chat', {
+  socket.emit('chat message', {
     message: message.value,
-    handle: handle.value
+    userName: userName.value
   });
+  output.innerHTML += '<p><strong>' + userName.value + ':</strong> ' + message.value + '</p>';
   message.value = '';
   e.preventDefault();
 });
 
 message.addEventListener('keypress', () => {
-  socket.emit('typing', handle.value);
+  socket.emit('typing', userName.value);
 });
 
-//Listen for events
-socket.on('chat', msg => {
+function tearDownForm(){
+  form.classList.add('hide')
+  startChatBtn.classList.remove('hide');
+}
+
+endChatBtn.addEventListener('click', () => {
+  output.innerHTML += `<p><em><strong>You</strong> have left the chat</em></p>`;
+  tearDownForm();
+  socket.emit('leave room');
+});
+
+
+//LISTEN for events
+socket.on('chat start' , chatInfo => {
+  [endChatBtn, chatContainer].forEach(element => element.classList.remove('hide'));
+  lookingForSomeone.innerHTML = "";
+  output.innerHTML = `<p><em><strong>${chatInfo.peerName || 'Someone'}</strong> has entered. Get the conversation going.</em></p>`;
   feedback.innerHTML = '';
-  output.innerHTML += '<p><strong>' + msg.handle + ':</strong> ' + msg.message + '</p>';
+});
+
+socket.on('chat end', function(data) {
+    output.innerHTML += `<p><em><strong>The other person</strong> has left the chat</em></p>`;
+    tearDownForm();
+});
+
+socket.on('chat message', msg => {
+  feedback.innerHTML = '';
+  output.innerHTML += '<p><strong>' + msg.userName + ':</strong> ' + msg.message + '</p>';
   chatWindow.scrollTop = chatWindow.scrollHeight;
 });
 
-socket.on('typing', userHandle => {
-  feedback.innerHTML = "<p><em>" + userHandle + ' is typing a message...</em></p>';
+socket.on('typing', userName => {
+  feedback.innerHTML = "<p><em>" + (userName || 'Someone') + ' is typing a message...</em></p>';
   chatWindow.scrollTop = chatWindow.scrollHeight;
 });
