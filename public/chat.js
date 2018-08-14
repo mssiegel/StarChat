8a0ee08bca72860934783701ad472829e50e309a
Moshe Siegel <siegel.moshes@gmail.com>
2018-08-13 21:00:51 -0400

    1. added css class to shrink buttons on small screens while a chat is in session, 2. centered all UI elements

--- public/chat.js
+++ public/chat.js
@@ -15,24 +15,42 @@ const suggestBtn = document.getElementById('suggest-btn');
       practiceModeBtn = document.getElementById('practice-mode-btn'),
       endChatBtn = document.getElementById('end-chat-btn'),
       sendMessageBtn = document.getElementById('send-message-btn'),
+      chatBtns = [startChatBtn, practiceModeBtn];
+      buttons = document.querySelectorAll('.btn');
       lookingForSomeone = document.getElementById('looking-for-someone');
 
-
 suggestBtn.addEventListener('click', suggestNewCharacter);
+if(!chosenChar.value) chatBtns.forEach(btn => {
+  btn.classList.add('disabled');
+  btn.disabled = true;
+});
 
 practiceModeBtn.addEventListener('click', () => {
   [form, chatContainer].forEach(element => element.classList.remove('hide'));
+  buttons.forEach(btn => btn.classList.add('small-btn'));
   output.innerHTML = `<p><em>You have entered </em><strong>PRACTICE MODE</strong><em>! Try out various characters, make your own stories, experiment...</em></p>`;
   message.focus();
   practiceModeBtn.classList.add('hide');
 });
 
-{//ensures both character input fields always display same values
+{
+  //1. ensures both character input fields always display same values
+  //2. disables chat buttons if character input field is blank
   userName.addEventListener('input',() => {
     chosenChar.value = userName.value;
   });
   chosenChar.addEventListener('input', () => {
     userName.value = chosenChar.value;
+    chatBtns.forEach(btn => {
+      if (chosenChar.value) {
+        btn.classList.remove('disabled');
+        btn.disabled = false;
+      }
+      else {
+        btn.classList.add('disabled');
+        btn.disabled = true;
+      }
+    });
   });
 }
 
@@ -57,6 +75,10 @@ function suggestNewCharacter() {
   } while (randomChar === userName.value);
   chosenChar.value = randomChar;
   userName.value = randomChar;
+  if(chosenChar.value) chatBtns.forEach(btn => {
+    btn.classList.remove('disabled');
+    btn.disabled = false;
+  });
   message.focus();
 }
 
@@ -87,6 +109,7 @@ endChatBtn.addEventListener('click', () => {
   output.innerHTML += `<p><em><strong>You</strong> have left the chat</em></p>`;
   tearDownForm();
   practiceModeBtn.classList.remove('hide');
+  buttons.forEach(btn => btn.classList.remove('small-btn'));
   socket.emit('leave room');
 });
 
@@ -94,6 +117,7 @@ endChatBtn.addEventListener('click', () => {
 //LISTEN for events
 socket.on('chat start' , peersName => {
   [form, endChatBtn, chatContainer].forEach(element => element.classList.remove('hide'));
+  buttons.forEach(btn => btn.classList.add('small-btn'));
   practiceModeBtn.classList.add('hide');
   lookingForSomeone.innerHTML = "";
   output.innerHTML = `<p><em><strong>${peersName || 'Someone'}</strong> has entered. Get the conversation going.</em></p>`;
@@ -103,6 +127,7 @@ socket.on('chat start' , peersName => {
 
 socket.on('chat end', function(data) {
     output.innerHTML += `<p><em><strong>Your peer</strong> has left the chat</em></p>`;
+    buttons.forEach(btn => btn.classList.remove('small-btn'));
     tearDownForm();
     practiceModeBtn.classList.remove('hide');
 });
